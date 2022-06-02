1391636b33490178c66f5f4634a5df1d5b3ecb62
Dennis Ojeda <royojeda1@gmail.com>
2022-06-02 13:54:50 +0800

    Add highlighting for board squares
    Change background color to green when highlighted

--- lib/square.rb
+++ lib/square.rb
@@ -1,20 +1,25 @@
 class Square
   attr_reader :rank, :file, :color
-  attr_accessor :occupant
+  attr_accessor :occupant, :highlighted
 
-  def initialize(rank:, file:, occupant: ' ', color:)
+  def initialize(rank:, file:, occupant: ' ', color:, highlighted: false)
     @rank = rank
     @file = file
     @occupant = occupant
     @color = color
+    @highlighted = highlighted
   end
 
   def to_s
-    case color
-    when 'light'
-      "\e[30;48;5;223m#{occupant.to_s}\e[0m"
-    when 'dark'
-      "\e[30;48;5;3m#{occupant.to_s}\e[0m"
+    if highlighted
+      "\e[30;48;5;84m#{occupant.to_s}\e[0m"
+    else
+      case color
+      when 'light'
+        "\e[30;48;5;223m#{occupant.to_s}\e[0m"
+      when 'dark'
+        "\e[30;48;5;3m#{occupant.to_s}\e[0m"
+      end
     end
   end
 end
