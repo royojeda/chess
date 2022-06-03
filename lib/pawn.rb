9764f0f22c58a62ca8f3ff2da2ef4f1978bc8d22
Dennis Ojeda <royojeda1@gmail.com>
2022-06-03 09:07:16 +0800

    Update Pawn moves
    Change direction based on color, check if first move to allow two moves
    forward.

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -1,9 +1,30 @@
 class Pawn
   attr_reader :color, :moves
+  attr_accessor :first_move
 
-  def initialize(color: 'white', moves: [[0, 1], [0, 2]])
+  def initialize(color:, first_move: true)
     @color = color
-    @moves = moves
+    @first_move = first_move
+    @moves = calculate
+  end
+
+  def calculate
+    case color
+    when 'white'
+      if first_move
+        self.first_move = false
+        [[0, 1], [0, 2]]
+      else
+        [[0, 1]]
+      end
+    when 'black'
+      if first_move
+        self.first_move = false
+        [[0, -1], [0, -2]]
+      else
+        [[0, -1]]
+      end
+    end
   end
 
   def to_s
