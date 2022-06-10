27e997e29ef0455af68a1cc92ee7a574d35f6083
Dennis Ojeda <royojeda1@gmail.com>
2022-06-10 15:56:47 +0800

    Add Pawn#captures
    En passant is not yet included.

--- lib/Pieces/pawn.rb
+++ lib/Pieces/pawn.rb
@@ -9,7 +9,8 @@ class Pawn < Piece
 
   def all_fins(board, square)
     arr = []
-    moves(board).each do |direction|
+
+    moves.each do |direction|
       direction.each do |move|
         break if board.out_of_bounds?(square.fin_from(move)) ||
                  !board.square_at(square.fin_from(move)).empty?
@@ -17,24 +18,43 @@ class Pawn < Piece
         arr << square.fin_from(move)
       end
     end
+
+    captures.each do |capture|
+      arr << square.fin_from(capture) if board.enemy_piece_at?(color, square.fin_from(capture))
+    end
+
     arr
   end
 
-  def moves(board)
-    # arr << [[0, "#{sign}1".to_i]] if board.enemy_at_front_left?
-    arr = [[0, 1]]
+  def moves
+    arr = [one_forward]
+
     if first_move
       self.first_move = false
-      arr << [0, 2]
+      arr << two_forward
     end
-    negate_direction(arr) if color == 'black'
+
     [arr]
   end
 
-  def negate_direction(coordinates)
-    coordinates.map! do |coordinate|
-      [-coordinate[0], -coordinate[1]]
-    end
+  def captures
+    [front_left, front_right]
+  end
+
+  def one_forward
+    color == 'white' ? [0, 1] : [0, -1]
+  end
+
+  def two_forward
+    color == 'white' ? [0, 2] : [0, -2]
+  end
+
+  def front_left
+    color == 'white' ? [-1, 1] : [1, -1]
+  end
+
+  def front_right
+    color == 'white' ? [1, 1] : [-1, -1]
   end
 
   def to_s
