7e44d305b7166fe0ae1d5f0fc40f572a5e9235d5
Dennis Ojeda <royojeda1@gmail.com>
2022-06-12 23:09:10 +0800

    Refactor Queen#moves

--- lib/Pieces/queen.rb
+++ lib/Pieces/queen.rb
@@ -2,26 +2,56 @@ require './lib/Pieces/piece'
 
 class Queen < Piece
   def moves
-    upward_moves = []
-    downward_moves = []
-    leftward_moves = []
-    rightward_moves = []
-    up_right_moves = []
-    up_left_moves = []
-    down_right_moves = []
-    down_left_moves = []
-    (1..7).each do |i|
-      upward_moves << [0, i]
-      downward_moves << [0, -i]
-      leftward_moves << [-i, 0]
-      rightward_moves << [i, 0]
-      up_right_moves << [i, i]
-      up_left_moves << [-i, i]
-      down_right_moves << [i, -i]
-      down_left_moves << [-i, -i]
-    end
     [upward_moves, downward_moves, leftward_moves, rightward_moves,
      up_right_moves, up_left_moves, down_right_moves, down_left_moves]
+  end
+
+  def upward_moves
+    (1..7).map do |i|
+      [0, i]
+    end
+  end
+
+  def downward_moves
+    (1..7).map do |i|
+      [0, -i]
+    end
+  end
+
+  def leftward_moves
+    (1..7).map do |i|
+      [-i, 0]
+    end
+  end
+
+  def rightward_moves
+    (1..7).map do |i|
+      [i, 0]
+    end
+  end
+
+  def up_right_moves
+    (1..7).map do |i|
+      [i, i]
+    end
+  end
+
+  def up_left_moves
+    (1..7).map do |i|
+      [-i, i]
+    end
+  end
+
+  def down_right_moves
+    (1..7).map do |i|
+      [i, -i]
+    end
+  end
+
+  def down_left_moves
+    (1..7).map do |i|
+      [-i, -i]
+    end
   end
 
   def color_white
