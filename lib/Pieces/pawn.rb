875e55be71489d561834eafcec5e4d4d0b2fa42e
Dennis Ojeda <royojeda1@gmail.com>
2022-06-16 22:33:06 +0800

    Refactor Pawn#moves

--- lib/Pieces/pawn.rb
+++ lib/Pieces/pawn.rb
@@ -1,6 +1,9 @@
 require './lib/Pieces/piece'
+require './lib/Modules/movable'
 
 class Pawn < Piece
+  include Movable
+
   def previous_is_two_forward?
     square.rank == (previous.rank.ord + 2).chr ||
       square.rank == (previous.rank.ord - 2).chr
@@ -29,29 +32,27 @@ class Pawn < Piece
   end
 
   def moves
-    arr = [one_forward]
-    arr << two_forward if first_move
-    [arr]
+    first_move ? two_forward : one_forward
   end
 
   def captures
-    [front_left, front_right]
+    front_left + front_right
   end
 
   def one_forward
-    color == 'white' ? [0, 1] : [0, -1]
+    color == 'white' ? [upward_moves(1)] : [downward_moves(1)]
   end
 
   def two_forward
-    color == 'white' ? [0, 2] : [0, -2]
+    color == 'white' ? [upward_moves(2)] : [downward_moves(2)]
   end
 
   def front_left
-    color == 'white' ? [-1, 1] : [1, -1]
+    color == 'white' ? up_left_moves(1) : down_right_moves(1)
   end
 
   def front_right
-    color == 'white' ? [1, 1] : [-1, -1]
+    color == 'white' ? up_right_moves(1) : down_left_moves(1)
   end
 
   def color_white
