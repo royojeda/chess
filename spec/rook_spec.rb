02175619ce4ed70c1bb528cfd76f238dc4af4ff1
Dennis Ojeda <royojeda1@gmail.com>
2022-06-18 00:22:43 +0800

    Refactor tests for Rook and Bishop
    Extracted elements of the expected array with 'let' blocks.

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -4,11 +4,12 @@ describe Rook do
   describe '#moves' do
     subject(:move_rook) { described_class.new(color: 'white') }
 
+    let(:upward_moves) { [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7]] }
+    let(:downward_moves) { [[0, -1], [0, -2], [0, -3], [0, -4], [0, -5], [0, -6], [0, -7]] }
+    let(:leftward_moves) { [[-1, 0], [-2, 0], [-3, 0], [-4, 0], [-5, 0], [-6, 0], [-7, 0]] }
+    let(:rightward_moves) { [[1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0]] }
+
     it 'returns an array of all proper rook moves' do
-      upward_moves = [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7]]
-      downward_moves = [[0, -1], [0, -2], [0, -3], [0, -4], [0, -5], [0, -6], [0, -7]]
-      leftward_moves = [[-1, 0], [-2, 0], [-3, 0], [-4, 0], [-5, 0], [-6, 0], [-7, 0]]
-      rightward_moves = [[1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0]]
       expected = [upward_moves, downward_moves, leftward_moves, rightward_moves].sort
       expect(move_rook.moves.sort).to eq(expected)
     end
