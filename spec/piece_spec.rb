c91be17aff41d207aba106379db473b981f43d09
Dennis Ojeda <royojeda1@gmail.com>
2022-06-18 22:56:29 +0800

    Add tests for Piece#location

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -1,4 +1,5 @@
 require './lib/Pieces/piece'
+require './lib/square'
 
 describe Piece do
   describe '#moved' do
@@ -7,6 +8,28 @@ describe Piece do
     it 'sets @first_move to false' do
       moved_piece.moved
       expect(moved_piece.first_move).to be(false)
+    end
+  end
+
+  describe '#location' do
+    subject(:location_piece) { described_class.new(color: 'white', square: test_square) }
+
+    context 'when the piece is at e2' do
+      let(:test_square) { instance_double(Square, file: 'e', rank: '2') }
+
+      it "returns an array of the piece's square's file and rank" do
+        expected = %w[e 2]
+        expect(location_piece.location).to eq(expected)
+      end
+    end
+
+    context 'when the piece is at b7' do
+      let(:test_square) { instance_double(Square, file: 'b', rank: '7') }
+
+      it "returns an array of the piece's square's file and rank" do
+        expected = %w[b 7]
+        expect(location_piece.location).to eq(expected)
+      end
     end
   end
 end
