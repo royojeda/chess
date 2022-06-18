d2fda6608a7e6bfbe4295e92d15a315fd03d253c
Dennis Ojeda <royojeda1@gmail.com>
2022-06-18 20:49:30 +0800

    Add tests for Pawn#specials

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -9,4 +9,24 @@ describe Pawn do
       expect(move_pawn.moves.sort).to eq(expected)
     end
   end
+
+  describe '#specials' do
+    context 'when the pawn is white' do
+      subject(:special_white_pawn) { described_class.new(color: 'white') }
+
+      it "returns the white pawn's capture moves" do
+        expected = [[-1, 1], [1, 1]].sort
+        expect(special_white_pawn.specials.sort).to eq(expected)
+      end
+    end
+
+    context 'when the pawn is black' do
+      subject(:special_black_pawn) { described_class.new(color: 'black') }
+
+      it "returns the black pawn's capture moves" do
+        expected = [[1, -1], [-1, -1]].sort
+        expect(special_black_pawn.specials.sort).to eq(expected)
+      end
+    end
+  end
 end
