a564fdbbc7f85566f228f0b0c9c2c7e5cd5e97ca
Dennis Ojeda <royojeda1@gmail.com>
2022-06-19 17:57:14 +0800

    Add tests for Board#rook_castle_move

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -43,4 +43,34 @@ describe Board do
       expect(test_destination).to have_received(:highlight_blue)
     end
   end
+
+  describe '#rook_castle_move' do
+    subject(:rook_castle_board) { described_class.new }
+
+    let(:test_source) { instance_double(Square) }
+    let(:test_destination) { instance_double(Square) }
+
+    before do
+      start = %w[a 1]
+      move = %w[d 1]
+      allow(rook_castle_board).to receive(:square_at).with(start).and_return(test_source)
+      allow(rook_castle_board).to receive(:square_at).with(move).and_return(test_destination)
+      allow(test_source).to receive(:store_as_previous)
+      allow(test_destination).to receive(:update_occupant)
+      allow(test_source).to receive(:remove_occupant)
+      rook_castle_board.rook_castle_move(move)
+    end
+
+    it 'sends #store_as_previous to the source square' do
+      expect(test_source).to have_received(:store_as_previous)
+    end
+
+    it 'sends #update_occupant to the destination square' do
+      expect(test_destination).to have_received(:update_occupant)
+    end
+
+    it 'sends #remove_occupant to the source square' do
+      expect(test_source).to have_received(:remove_occupant)
+    end
+  end
 end
