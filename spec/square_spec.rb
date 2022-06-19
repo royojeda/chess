ee4c568f5020b12450cf0e1650abe508a18b36cf
Dennis Ojeda <royojeda1@gmail.com>
2022-06-19 12:33:08 +0800

    Add tests for Square#can_castle?

--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -1,5 +1,6 @@
 require './lib/square'
 require './lib/Pieces/pawn'
+require './lib/Pieces/rook'
 
 describe Square do
   describe '#place' do
@@ -39,6 +40,49 @@ describe Square do
       update_square.update_occupant(test_source)
       expected = test_piece
       expect(update_square.occupant).to eq(expected)
+    end
+  end
+
+  describe '#can_castle?' do
+    subject(:castle_square) { described_class.new(file: 'a', rank: '1') }
+
+    context "when @occupant is a rook of the same color(as the argument) that hasn't moved yet" do
+      let(:test_rook) { instance_double(Rook, color: 'white', first_move: true) }
+
+      before do
+        allow(castle_square).to receive(:occupant).and_return(test_rook)
+        allow(test_rook).to receive(:is_a?).with(Rook).and_return(true)
+      end
+
+      it 'returns true' do
+        expect(castle_square.can_castle?('white')).to be(true)
+      end
+    end
+
+    context 'when @occupant is a rook of the same color(as the argument) that has already moved' do
+      let(:test_rook) { instance_double(Rook, color: 'white', first_move: false) }
+
+      before do
+        allow(castle_square).to receive(:occupant).and_return(test_rook)
+        allow(test_rook).to receive(:is_a?).with(Rook).and_return(true)
+      end
+
+      it 'returns false' do
+        expect(castle_square.can_castle?('white')).to be(false)
+      end
+    end
+
+    context 'when @occupant is not a rook of the same color(as the argument)' do
+      let(:test_occupant) { instance_double(Pawn, color: 'white', first_move: true) }
+
+      before do
+        allow(castle_square).to receive(:occupant).and_return(test_occupant)
+        allow(test_occupant).to receive(:is_a?).with(Rook).and_return(false)
+      end
+
+      it 'returns false' do
+        expect(castle_square.can_castle?('white')).to be(false)
+      end
     end
   end
 end
